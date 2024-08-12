5a0a9b97c1ad144eea93e11100d17b6a00d92285
pillowyork1 <ce.zzy@hotmail.com>
2024-08-11 22:44:00 -0300

    Adicionado projeto SendReceive

---
+++ GRPCTestEnvioRecebimento/Program.cs
@@ -0,0 +1,31 @@
+﻿using Grpc.Net.Client;
+using Proto.Greet;
+
+namespace GRPCTestSendReceive
+{
+    public class Program
+    {
+        public static async Task Main(string[] args)
+        {
+            Console.WriteLine("Iniciando GRPC Test SendReceive");
+            Console.WriteLine("Realizando um Request");
+
+            // Create a gRPC channel and client
+            GrpcChannel channel = GrpcChannel.ForAddress("http://localhost:5000");
+            Greeter.GreeterClient client = new Greeter.GreeterClient(channel);
+
+
+            for (int cont = 0; cont <= 99999; cont++)
+            {
+                var request = new HelloRequest { Name = "World" };
+
+                // Call the SayHello method
+                var reply = await client.SayHelloAsync(request);
+
+                // Display the reply message in the console
+                Console.WriteLine($"Response {cont}: {reply.Message}");
+            }
+
+        }
+    }
+}
